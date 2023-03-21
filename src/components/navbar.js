8b2b23829a2f5d6382f2d34c24f2fc747b1a92b2
kait <kingkittykait@gmail.com>
2023-03-20 23:07:36 -0700

    got a good start on some of the components

---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+
+export default function Navbar() {
+    return (
+        <div class="hero head">
+            <nav class="navbar" role="navigation">
+                <a class="navbar-item">
+                    About
+                </a>
+                <a class="navbar-item">
+                    Projects
+                </a>
+                <a class="navbar-item">
+                    Links
+                </a>
+
+            </nav>
+        </div>
+    )
+}
---
+++ src/components/projects.js
@@ -0,0 +1,15 @@
+
+export default function Projects() {
+    return (
+        <div class="box">
+            <div class="content">
+                <p>
+                    <strong>Projects</strong>
+                    <br>
+                    <strong>Spoopy</strong>
+                    </br>
+                </p>
+            </div>
+        </div>
+    )
+}
